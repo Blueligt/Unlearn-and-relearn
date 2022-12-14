24a89994e7f075ab97e5e650f89a15033156494b
Blueligt <ekenevic70@gmail.com>
2022-12-14 12:21:35 +0100

    A save URL ext

---
+++ chrome_ext/README.md
@@ -0,0 +1,5 @@
+# A save-tab chrome ext
+
+    1. Saves inputted URl unto an array which can be launched anytime in the future
+    2. Saves the current tab opened in the browser
+    3. Able to delete saved URLs
--- chrome_ext/chrome_ext.js
+++ chrome_ext/chrome_ext.js
@@ -1,44 +1,32 @@
+// Accessing the DOM
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const saveBtn = document.getElementById("input-btn")
 const delBtn = document.getElementById("delete-btn")
 const saveTab = document.getElementById("tab-btn")
 const ulEl = document.getElementById("ul-el")
-
 const leadsFromLocalStorage = JSON.parse( (localStorage.getItem("myLeads")))
 console.log(leadsFromLocalStorage)
+
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
     render(myLeads)
 }
 
-// const tabs = [
-//     {url: "www.google.com"}
-// ]
-
+// Grabbing current tab
 saveTab.addEventListener('click', function() {
-    // chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    //     myLeads.push(tabs[0].url)
-    //     localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    //     render(myLeads)
-    // })
-
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         myLeads.push(tabs[0].url)
         localStorage.setItem("myLeads", JSON.stringify(myLeads))
         render(myLeads)
     })
-    // console.log(tabs[0].url)
-    // myLeads.push(tabs[0].url)
-    // localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    // render(myLeads)
 })
 
+// Appending URL to array
 function render(leads) {
     let listItems = ""
     for (let i = 0; i < leads.length; i++) {
-        // listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
         listItems += `
             <li>
                 <a target='_blank' href='${leads[i]}'>
@@ -46,36 +34,22 @@ function render(leads) {
                 </a>
             </li>
         `
-    
-        // const li = document.createElement("li")
-        // li.textContent = myLeads[i]
-        // ulEl.append(li)
     }
     ulEl.innerHTML = listItems
 }
 
+// Delete URL button
 delBtn.addEventListener('dblclick', function() {
     localStorage.clear()
     myLeads = []
     render(myLeads)
 })
+
+// Save URl button
 saveBtn.addEventListener('click', function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
     localStorage.setItem("myLeads", JSON.stringify(myLeads))
 
     render(myLeads)
-    // console.log(localStorage.getItem("myLeads"))
 })
-
-
-
-
-
-
-// let tryBox = document.getElementById("try")
-// let openBox = document.getElementById("open")
-
-// openBox.addEventListener('click', function() {
-//     tryBox.textContent = "You have opened the box"
-// })
